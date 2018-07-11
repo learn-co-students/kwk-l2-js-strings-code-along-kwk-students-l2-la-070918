36fb2414543fd8815f76791b21042437ea4b4ca2
jenrose24 <rosienifer@icloud.com>
2018-07-11 20:43:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // Write your code in this file!
+const currentUser = "Karlie";
+
+const welcomeMessage=`Welcome to Klossybook, ${currentUser}!`;
+
+const excitedWelcomeMessage= welcomeMessage.toUpperCase();
+
+const shortGreeting =`Welcome, ${currentUser.charAt(0)}!`;
+shortGreeting ;
+
+
+
 
